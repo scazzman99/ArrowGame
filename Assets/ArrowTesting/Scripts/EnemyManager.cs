c438dcb52e84784b3a8f5cede8ef225bed0c355c
threepat1 <42788704+threepat1@users.noreply.github.com>
2018-09-19 14:36:37 +1000

    add enemy manager script

--- Assets/ArrowTesting/Scripts/EnemyManager.cs
+++ Assets/ArrowTesting/Scripts/EnemyManager.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
+    private Gameobject enemySpawn;
+
+    public vector3 center;
+    public vector3 size;
+
 
 	// Use this for initialization
 	void Start () {
+        SpawnEnemy();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (enemySpawn = 0)
+            SpawnEnemy();
 	}
+
+    public void SpawnEnemy()
+    {
+        Vector3 pos = center + new vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+
+        Instantiate(enemySpawn, pos, Quaternion.identity);
+    }
+    void onDrawGizmosSelected()
+    {
+        Gizmos.DrawCube(center, size);
+    }
 }
