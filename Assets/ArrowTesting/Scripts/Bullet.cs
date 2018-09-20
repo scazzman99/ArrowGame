480d89e57d802aeb2b34542440b0e27ab3fa6381
jtojung <42223493+jtojung@users.noreply.github.com>
2018-09-20 18:24:10 +1000

    add enemyshoot and bullet

---
+++ Assets/ArrowTesting/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float speed;
+    public float maxDistance;
+
+    private float damage;
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        maxDistance += 1 * Time.deltaTime;
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            //player.GetComponent<PlayerPrefs>().health -= 20;
+        }
+    }
+}
---
+++ Assets/ArrowTesting/Scripts/Enemyshoot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyshoot : MonoBehaviour {
+    public float pointsToGive;
+    public float waitTime;
+    private float currentTime;
+    private bool shot;
+
+    public float health;
+    public GameObject bullet;
+    public GameObject player;
+    public GameObject bulletSpawnpoint;
+    public Transform bulletSpawned;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+        bulletSpawnpoint = GameObject.Find("PistolHolder/spawnPoint");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(health <=0)
+        {
+            Die();
+        }
+        this.transform.LookAt(player.transform);
+
+        if (currentTime == 0)
+            Shoot();
+        if (shot && currentTime < waitTime)
+            currentTime += 1 * Time.deltaTime;
+        if (currentTime >= waitTime)
+            currentTime = 0;
+    }
+    public void Shoot()
+        {
+            shot = true;
+        bulletSpawned = Instantiate(bullet.transform, bulletSpawnpoint.transform.position, Quaternion.identity);
+        bulletSpawned.rotation = this.transform.rotation;
+        }
+    public void Die()
+    {
+        Destroy(this.gameObject);
+        // player.GetComponent<PlayerPrefs>().points += pointsToGive; 
+    }
+	
+}
