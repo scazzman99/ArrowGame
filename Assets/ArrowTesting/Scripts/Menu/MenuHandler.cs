bb5df421f8372b9e89de938d1b85507205c9dc01
jtojung <42223493+jtojung@users.noreply.github.com>
2018-09-25 18:21:56 +1000

    add menu

---
+++ Assets/ArrowTesting/Scripts/Menu/MenuHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+
+    #region Variables
+    public GameObject mainMenu, optionsMenu;
+    public bool showOptions;
+
+    #endregion
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Loadgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exitgame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+        Application.Quit();
+
+    }
+    bool OptionToggle()
+    {
+        if (showOptions)//showOptions == true means showOptions is true
+        {
+            showOptions = false;
+            mainMenu.SetActive(true);
+            optionsMenu.SetActive(false);
+            return false;
+        }
+        else
+        {
+            showOptions = true;
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+            return true;
+        }
+    }
+}
